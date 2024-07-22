a8be879dac1ce0782394f62e95ef516dde86a112
c2kim <c2kim@uwaterloo.ca>
2024-07-22 18:00:24 -0400

    player updates

---
+++ characters/player.h
@@ -0,0 +1,30 @@
+#ifndef _PLAYER_H_
+#define _PLAYER_H_
+
+#include "characters/character.h"
+#include "tile.h";
+#include "potionfx.h";
+#include "potion.h";
+
+class Player : public Character {
+    int hp;
+    int maxHp;
+    int atk;
+    int def; 
+    int initAtk;
+    int initDef;
+    Potionfx* pfx;
+    void onDeath();
+
+   public:
+    Player(int hp, int maxHp, int atk, int def, Potionfx* pfx);
+    void applyPotion(Potion& p);
+    void increaseGold(float g);
+    void gainCompass();
+    void resetTempFx();
+    void gainBarrierSuit();
+    void calculateScore();
+    void use(pair<int, int> pcoords, Tile& t);
+};
+
+#endif
